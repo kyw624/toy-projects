e80745b2b4ceb3878bbbc86107cd14284eae31f7
Y.W KIM <kyw624@gmail.com>
2024-02-22 01:54:47 +0900

    Feat: 각 페이지별 라우트 설정

--- react_basic-board-app/src/App.jsx
+++ react_basic-board-app/src/App.jsx
@@ -1,7 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from '@components/UI/RootLayout';
+import ErrorPage from '@pages/ErrorPage';
+import PostListPage from '@pages/PostListPage';
+import PostDetailPage from '@pages/PostDetailPage';
+import CreatePostPage from '@pages/CreatePostPage';
+import EditPostPage from '@pages/EditPostPage';
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/board',
+        element: <PostListPage />,
+      },
+      {
+        path: '/board/:postId',
+        element: <PostDetailPage />,
+      },
+      {
+        path: '/board/new',
+        element: <CreatePostPage />,
+      },
+      {
+        path: '/board/:postId/edit',
+        element: <EditPostPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
